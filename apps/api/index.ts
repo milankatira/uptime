803a120b6f43a5dec338226292d9c40c10fd194c
milankatira <milankatira26@gmail.com>
2025-05-03 20:45:02 +0530

    feat(api): add authentication middleware and JWT support
    Introduce JWT-based authentication middleware to secure API endpoints. Add necessary dependencies, including Express and jsonwebtoken, and define a custom Express Request type to include userId. Configure JWT public key and implement middleware to verify tokens and attach userId to the request object. Update API endpoints to use the middleware for protected routes.

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,84 @@
-console.log("Hello via Bun!");
+import { prismaClient } from 'db/client';
+
+import express from 'express';
+import { authMiddleware } from './middleware';
+const app = express();
+
+app.post('/api/v1/website', async (req, res) => {
+    const userId = req.userId!;
+    const { url } = req.body;
+
+    const data = await prismaClient.website.create({
+        data: {
+            userId,
+            url,
+        },
+    });
+
+    res.json({
+        id: data.id,
+    });
+});
+
+app.get(
+    '/api/v1/website/status',
+    authMiddleware as any,
+    async (req: express.Request, res: express.Response): Promise<void> => {
+        const websiteId = req.query.websiteId! as unknown as string;
+        const userId = req.userId;
+
+        const data = await prismaClient.website.findFirst({
+            where: {
+                id: websiteId,
+                userId,
+                disabled: false,
+            },
+            include: {
+                ticks: true,
+            },
+        });
+
+        res.json(data);
+    }
+);
+
+app.get('/api/v1/websites', authMiddleware as any, async (req, res) => {
+    const userId = req.userId!;
+
+    const websites = await prismaClient.website.findMany({
+        where: {
+            userId,
+            disabled: false,
+        },
+        include: {
+            ticks: true,
+        },
+    });
+
+    res.json({
+        websites,
+    });
+});
+
+app.delete('/api/v1/website', authMiddleware as any, async (req, res) => {
+    const websiteId = req.body.websiteId;
+    const userId = req.userId!;
+
+    await prismaClient.website.update({
+        where: {
+            id: websiteId,
+            userId,
+        },
+        data: {
+            disabled: true,
+        },
+    });
+
+    res.json({
+        message: 'Deleted website successfully',
+    });
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080 🚀');
+});
