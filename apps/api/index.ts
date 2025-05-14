a2aa5a72e41432930f812a633d4b40438c1bb19f
milankatira <milankatira26@gmail.com>
2025-05-14 07:38:09 -0700

    feat(api): add incident management routes, services, and controllers
    Introduce new routes for managing incidents, including CRUD operations. Add corresponding services and controllers to handle incident data retrieval, updates, and deletions. Include an enum for incident status in the types directory.

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import websiteRoutes from "./routes/websiteRoutes";
 
 import "./types/express.d.ts";
+import incidentRoutes from "./routes/incidentRoutes.ts";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", websiteRoutes);
+app.use("/api/v1", incidentRoutes);
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080 🚀");
